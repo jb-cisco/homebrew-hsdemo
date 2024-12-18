3d8c312198247249ced35b9688c4574655034f06
JB <jamboyki@cisco.com>
2024-12-18 14:43:47 -0500

    2nd

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os/exec"
 )
 
 func main() {
@@ -9,7 +10,7 @@ func main() {
 	var age int
 
 	// Prompt for the user's name
-	fmt.Print("Enter your name: ")
+	fmt.Print("Enter your name2: ")
 	fmt.Scanln(&name)
 
 	// Prompt for the user's age
@@ -18,4 +19,18 @@ func main() {
 
 	// Print a greeting message
 	fmt.Printf("Hello, %s! You are %d years old.\n", name, age)
+
+	// Define the command to run
+	cmd := exec.Command("eksctl", "delete", "cluster", "jimmy-aws-c1")
+
+	// Run the command and capture the output
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		// If there is an error, print it and exit
+		fmt.Printf("Error executing command: %s\n", err)
+		return
+	}
+
+	// Print the output of the command
+	fmt.Printf("Command output:\n%s\n", output)
 }
