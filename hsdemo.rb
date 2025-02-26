c48dbb834aced2cb75c5cfa8c966e655f5518525
JB <jamboyki@cisco.com>
2025-02-26 14:05:00 -0500

    bump version

--- hsdemo.rb
+++ hsdemo.rb
@@ -1,6 +1,6 @@
 class Hsdemo < Formula
     desc "Sets up hs demo in eks cluster"
-    version "1.3"
+    version "1.3.1"
     url "https://github.com/jb-cisco/homebrew-hsdemo/releases/download/v1.3/hsdemo" 
     # List of dependencies
   depends_on "eksdemo"
